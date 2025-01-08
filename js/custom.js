776ea5bdc21f1ade64b812139494e41c95ca96b4
Auzers <1058564630@qq.com>
2025-01-08 17:04:38 +0800

    Deployed 653133f with MkDocs version: 1.6.1

--- js/custom.js
+++ js/custom.js
@@ -22,3 +22,23 @@ $(document).ready(function () {
         $(value).simpleLightbox()
     }
 })
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     添加反馈部分到页面底部
+//     const feedbackSection = `
+//         <div style="text-align: center; padding: 20px;">
+//             <h3>激励</h3>
+//             <p>难道说……你愿意给我买一瓶快乐水吗！🫣</p>
+//             <img src="https://s2.loli.net/2023/08/03/EGbIvMQXalKTsUi.png" width="200px">
+//             <br>
+//             <button style="font-size: 24px; padding: 10px;">👍</button>
+//         </div>
+//     `;
+//     const footer = document.querySelector('footer');
+//     if (footer) {
+//         footer.insertAdjacentHTML('beforeend', feedbackSection);
+//     }
+
+//     触发 MathJax 渲染
+//     MathJax.typesetPromise();
+// });
